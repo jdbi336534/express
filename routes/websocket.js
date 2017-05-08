1ba3631d2779cc975e214bc18442e5368c052636
oliver <992042912@qq.com>
2017-05-08 09:49:54 +0800

    修改了websocket断开连接报错bug

--- routes/websocket.js
+++ routes/websocket.js
@@ -2,40 +2,37 @@ var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 var util = require('util');
-// app.use(express.static('./static'));
-expressWs.getWss().on('connection', function(ws) {
+var clientMap = new Object();
+var i = 0;
+
+expressWs.getWss().on('connection', function (ws) {
     console.log('connection open');
 });
-
-app.ws('/ws', function(ws, req) {
+app.ws('/ws', function (ws, req) {
     util.inspect(ws);
-    ws.on('message', function(msg) {
-        console.log('_message');
-        console.log(msg);
-        ws.send('fuck');
+    ws.name=++i;
+    clientMap[ws.name]=ws;
+    ws.on('message', function (msg) {
+        console.log('接收到数据：',msg);
     });
-    ws.on('open', function(msg) {
-        console.log('_open');
-        console.log(msg);
-        ws.send('open:' + msg);
+    ws.on('open', function (msg) {
+        ws.send('open:', msg);
     });
-    ws.on('close', function(msg) {
-        console.log('_close');
-        console.log(msg);
-        ws.send('close:' + msg);
+    ws.on('close', function (msg) {
+        console.log('_close,有人离开:', msg);
+         delete clientMap[ws.name];
     });
-    ws.on('error', function(msg) {
-        console.log('_error');
-        console.log(msg);
-        ws.send('error:' + msg);
+    ws.on('error', function (msg) {
+        console.log('_error:', msg);
     });
 });
+var k=0;
 var aWss = expressWs.getWss('/ws');
-var i = 0;
-setInterval(function() {
-    aWss.clients.forEach(function(client) {
-        client.send('hello' + i);
-        i++
+setInterval(function () {
+    // console.log('aWss.clients:',aWss.clients);
+    aWss.clients.forEach(function (client) {
+        client.send('the '+k+'times send data:'+k);
+        k++;
     });
 }, 1000);
 app.listen(3001);
