1404b4136590fcc94a081e7b6a9b928f017d45d1
oliver <992042912@qq.com>
2017-05-19 09:53:48 +0800

    www.eleps.cn证书ok

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    if (req.protocol === 'https') {
+        res.render('index', {
+            title: 'https'
+        });
+    } else {
+        res.render('index', {
+            title: 'Express'
+        });
+    }
+
 });
 
 module.exports = router;
